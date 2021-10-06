215b1e697a595ef2c6bf10c66053d35bf3c6bf8e
Ghanshyam Kandu <1694gk@gmail.com>
2021-10-06 19:33:17 +0530

    we just add the action creator with the help of redux-thunk

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,10 @@
-export const fetchPosts = () => {
-    return{
-        type: 'FETCH_POST'
+import JasonPlaceHolder from "../api/JasonPlaceHolder";
+
+export const fetchPosts =  () => {
+    return async (dispatch) => {
+        const response = await JasonPlaceHolder.get('/posts');
+
+        dispatch({ type: 'FETCH_POSTS', payload: response})
     };
+    
 };
